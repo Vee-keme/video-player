be6bc6d9c231b49782cadc5e620841a9084f8bce
Vee-keme <veliaprecious@gmail.com>
2023-05-01 22:38:57 +0100

    add video card

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -15,6 +15,6 @@ const options = {
 };
 
 export const fetchFromAPI = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}, options `);
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
   return data;
 };
